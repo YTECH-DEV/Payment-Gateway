a0d2befb4528e24a7de1c8b0dc9a4723fef5898a
Yaro Bekassoe Emmanuel <emmayaro71@gmail.com>
2025-10-22 18:53:17 +0000

    Add modal and payment gateway HTML, update JS entry
    Added modal.html and index2.html for modal overlay and payment gateway UI. Replaced cdn/methods/main.js with a new cdn/main.js entry point, updating import/export structure.

--- cdn/methods/main.js
+++
@@ -1,6 +0,0 @@
-import { paymentConfig } from './payment-config';
-import { ypay } from './ypay';
-import {lang} from './lang';
-import {formController} from "./form_controller";
-
-export default {paymentConfig, ypay, lang, formController};
